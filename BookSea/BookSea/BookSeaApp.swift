32d1d0b9b2734050b1a1052ba0b7fc26e9c872ef
Viennarz Curtiz <viennarzc@gmail.com>
2022-01-26 08:41:27 +0800

    Create Project

---
+++ BookSea/BookSea/BookSeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSeaApp.swift
+//  BookSea
+//
+//  Created by Viennarz Curtiz on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BookSeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
