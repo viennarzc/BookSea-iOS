7feed052ad5b02e7c0a77636b0d94ca8e59a015e
Viennarz Curtiz <viennarzc@gmail.com>
2022-01-26 10:34:12 +0800

    Add API service, hook data

--- BookSea/BookSea/BookSeaApp.swift
+++ BookSea/BookSea/BookSeaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BookSeaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: ViewModel(api: BooksAPI()))
         }
     }
 }
